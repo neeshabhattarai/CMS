725a09543f53d9fdfb02912b8c5ffe4977905f18
Neesha <nishabhattarai778@gmail.com>
2025-09-01 22:48:56 +0545

    added app.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,4 +1,4 @@
 const serverless = require('serverless-http');
-const app = require('./app'); // your express app
+const app = require('../app'); // your express app
 
 module.exports.handler = serverless(app);
