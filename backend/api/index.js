dbbf6e54026cab1ea89487390adfdfb899b9a194
Neesha <nishabhattarai778@gmail.com>
2025-09-01 17:10:47 +0545

    backend setup change

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,4 +1,3 @@
 const serverless = require("serverless-http");
-const app = require("../app"); // your existing Express app
-
+const app = require("../app"); 
 module.exports.handler = serverless(app);
